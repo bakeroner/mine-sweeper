c9156c89e27f2e2f9e1fd03437003c6f75c21439
AM_PC <ashakhtorin@inbox.ru>
2018-10-22 21:15:26 +0300

    minor changes

--- js/game-js.js
+++ js/game-js.js
@@ -13,8 +13,6 @@ class MineGame {
 		this.fieldWidth = fieldWidth;
 		this.areaNearCurrentTarget = [];
 		this.minesNear = 0;
-		this.targetX = 0;
-		this.targetY = 0;
 		this.storage = {
 		minesNumber: minesNumber,
 		flagsNumber: minesNumber,
@@ -27,16 +25,12 @@ class MineGame {
 		/*building field by using variables*/
 		/*indexing field in some way*/
 		/*setting mines*/
-		let minesCounter = this.storage.minesNumber;
-		let randomNumber = 0;
 		let flagSet = document.createElement("p");
-			flagSet.innerHTML = this.storage.flagsNumber;
-			minesIndicator.appendChild(flagSet);
-
-			let timerSet = document.createElement("p");
-			timerSet.innerHTML = this.storage.timer;
-			timerIndicator.appendChild(timerSet);
-			//console.log(game.storage.flagsNumber);
+		flagSet.innerHTML = this.storage.flagsNumber;
+		minesIndicator.appendChild(flagSet);
+		let timerSet = document.createElement("p");
+		timerSet.innerHTML = this.storage.timer;
+		timerIndicator.appendChild(timerSet);
 
 		for (let i = 0; i<this.fieldWidth; i++) {
 			for(let j = 0; j<this.fieldHeight; j++) {
@@ -47,47 +41,44 @@ class MineGame {
 				gameField.appendChild(item);
 			}
 		}
-			while (minesCounter>0) {//!!!mix it with top part in some way
-				for (let i = 0; i<this.fieldWidth; i++) {
-					for (let j = 0; j<this.fieldHeight; j++) {
-						randomNumber = Math.floor((Math.random() * 10) + 1);
-						if (randomNumber == 10 && minesCounter>0) {
-							let item = document.querySelector('[data-x=' + '"' + j + '"' + '][data-y=' + '"' + i + '"' + ']');
-							if (!item.firstChild) {
+		let minesCounter = this.storage.minesNumber;
+		while (minesCounter>0) {
+			for (let i = 0; i<this.fieldWidth; i++) {
+				for (let j = 0; j<this.fieldHeight; j++) {
+					let randomNumber = Math.floor((Math.random() * 10) + 1);
+					if (randomNumber == 10 && minesCounter>0) {
+						let item = document.querySelector('[data-x=' + '"' + j + '"' + '][data-y=' + '"' + i + '"' + ']');
+						if (!item.firstChild) {
 							let currentPosition = {x: j, y: i};
+							this.storage.minesPlacement.push(currentPosition);
 							let mineIco = document.createElement("img");
 							mineIco.setAttribute("src", "images/bomb.png");
 							mineIco.classList.add("bombImage");
 							mineIco.classList.add("hideElement"); 
 							item.appendChild(mineIco);
 							minesCounter--;
-							this.storage.minesPlacement.push(currentPosition);
-							}
 						}
+					}
 				}
 			}		
 		}
 	}
-
-		areaNearCurrentTargetCalculate (cellX, cellY) {
+		neighboursCheck (cellX, cellY) {
 		this.areaNearCurrentTarget = [];
 		let minesArray = this.storage.minesPlacement;
 		this.minesNear = 0;
 		let minesNearby = this.minesNear;
 		for (let i = cellX-1; i<=cellX+1; i++) {//One day i'll use jQuery for it
 			for (let j = cellY-1; j<=cellY+1; j++) {
-				//console.log("cellX: " + i + " cellY: " + j);
 				let currentCell = {x: i, y: j};
 				let elementNearby = document.querySelector('[data-x=' + '"' + i + '"' + '][data-y=' + '"' + j + '"' + ']');
-				//console.log(elementNearby);
-					this.areaNearCurrentTarget.push(elementNearby);
+				this.areaNearCurrentTarget.push(elementNearby);
 			}
 		}	
 		this.areaNearCurrentTarget.forEach(function (elem) {
 			for (let i = 0; i<minesArray.length; i++) {
 				if (elem && +elem.dataset.x == minesArray[i].x && +elem.dataset.y == minesArray[i].y) {// [4] is array center
 					minesNearby++;
-					//console.log("mines from function: " + minesNearby);	
 				}
 			}
 		})
@@ -100,14 +91,12 @@ class MineGame {
 		}
 	}
 	cellOpener (currentTarget) {
-		/*open cell show mines number open empty cells*/
+		/*open first cell show mines number open empty cells*/
 		if (currentTarget.classList.contains("cellClass"))
 		{
-			this.targetX = +currentTarget.dataset.x;
-			this.targetY = +currentTarget.dataset.y;
 			currentTarget.classList.remove("cellClass");
 			currentTarget.classList.add("cellClassOpen");
-			if (!this.areaNearCurrentTargetCalculate(+currentTarget.dataset.x, +currentTarget.dataset.y)) {
+			if (!this.neighboursCheck(+currentTarget.dataset.x, +currentTarget.dataset.y)) {
 				let item = document.createElement("p");
 				item.classList.add("cellText");
 				item.innerHTML = this.minesNear;
@@ -120,14 +109,14 @@ class MineGame {
 			}
 		}
 	}
-	emptyCellOpener () {//!!!check and rewrite
+		emptyCellOpener () {//!!!check and rewrite
 		this.areaNearCurrentTarget.splice(4,1);
 		let cellNeighbours = this.areaNearCurrentTarget;
 		//console.log("cellNeighbours: " + cellNeighbours);
 		for (let i = 0; i<cellNeighbours.length; i++) {
 			if (cellNeighbours[i]) {
 				//console.log("x: " + cellNeighbours[i].dataset.x + " y: " + cellNeighbours[i].dataset.y);
-				if (!this.areaNearCurrentTargetCalculate(+cellNeighbours[i].dataset.x, +cellNeighbours[i].dataset.y)) {
+				if (!this.neighboursCheck(+cellNeighbours[i].dataset.x, +cellNeighbours[i].dataset.y)) {
 					//console.log("mines: " + this.minesNear);
 					cellNeighbours[i].classList.remove("cellClass");
 					cellNeighbours[i].classList.add("cellClassOpen");
@@ -136,7 +125,7 @@ class MineGame {
 					item.innerHTML = this.minesNear;
 						cellNeighbours[i].appendChild(item);
 				}
-				else if (this.areaNearCurrentTargetCalculate(+cellNeighbours[i].dataset.x, +cellNeighbours[i].dataset.y)) {
+				else if (this.neighboursCheck(+cellNeighbours[i].dataset.x, +cellNeighbours[i].dataset.y)) {
 					if (!cellNeighbours[i].classList.contains("blank")) {
 						cellNeighbours[i].classList.remove("cellClass");
 						cellNeighbours[i].classList.add("blank");
@@ -165,17 +154,13 @@ class MineGame {
 			}
 		}
 	}
-	winCheck () {
+	winOrDie (currentTargetX, currentTargetY, isItLeft) {
 		/*checking end of the game showing Congratulation Menu*/
 		if (this.storage.correctFlags == this.storage.minesNumber && this.storage.flagsNumber == 0 && !document.querySelector('.cellClass:not(.flag)')) {
-		alert("That's all, you won!" + " Your time: " + this.storage.timer);
+			alert("That's all, you won!" + " Your time: " + this.storage.timer);
 		}
-	}
-	loseCheck () {
-		let currentTargetX = this.targetX;
-		let currentTargetY = this.targetY;
 		this.storage.minesPlacement.forEach(function (elem) {
-			if (elem.x == currentTargetX && elem.y == currentTargetY) {
+			if (elem.x == currentTargetX && elem.y == currentTargetY && isItLeft) {
 				document.querySelectorAll('.bombImage').forEach (function (elem) {
 					elem.classList.toggle('hideElement');
 				})
@@ -219,8 +204,7 @@ gameField.addEventListener("click", function (event) {
 	/*using event delegation on field elements to open*/
 	let target = event.target;
 	game.cellOpener(target);
-	game.loseCheck();
-	game.winCheck();//check it
+	game.winOrDie(+target.dataset.x, +target.dataset.y, true);
 })
 gameField.addEventListener("contextmenu", function (event) { //!!!rewrite all
 	/*using event delegation on field elements to set flag*/
@@ -240,8 +224,7 @@ gameField.addEventListener("contextmenu", function (event) { //!!!rewrite all
 			})
 			game.storage.flagsNumber--;
 			minesIndicator.children[0].innerHTML = game.storage.flagsNumber;
-			//console.log(game.storage.flagsNumber);
-			game.winCheck();
+			game.winOrDie(+target.dataset.x, +target.dataset.y, false);//think about it
 		}
 		else {
 			//remove flag
@@ -263,6 +246,5 @@ gameField.addEventListener("contextmenu", function (event) { //!!!rewrite all
 		game.storage.flagsNumber++;
 		minesIndicator.children[0].innerHTML = game.storage.flagsNumber;		
 	}
-	console.log("Right click catch");
 	return false;
 })
